fe70f94fe579f066817b63d3c147c0a098e21e3b
Daniel Mundra <daniel.mundra@civicactions.com>
2023-06-23 14:32:31 -0700

    Updated to support ILIAS 7 and ran code cleanup.

--- classes/class.ilGoogleAnalyticsConfigGUI.php
+++ classes/class.ilGoogleAnalyticsConfigGUI.php
@@ -11,105 +11,105 @@ include_once("./Services/Component/classes/class.ilPluginConfigGUI.php");
 class ilGoogleAnalyticsConfigGUI extends ilPluginConfigGUI
 {
 
-	/**
-	 * Handles all commmands, default is 'configure'
-	 *
-	 * @access public
-	 */
-	public function performCommand($cmd)
-	{
-		switch ($cmd) {
-			case 'configure':
-			case 'save':
-				$this->$cmd();
-				break;
-		}
-	}
+    /**
+     * Handles all commmands, default is 'configure'
+     *
+     * @access public
+     */
+    public function performCommand($cmd)
+    {
+        switch ($cmd) {
+            case 'configure':
+            case 'save':
+                $this->$cmd();
+                break;
+        }
+    }
 
 
-	/**
-	 * Configure screen
-	 *
-	 * @access public
-	 */
-	public function configure()
-	{
-		global $DIC;
+    /**
+     * Configure screen
+     *
+     * @access public
+     */
+    public function configure()
+    {
+        global $DIC;
 
-		/**
-		 * @var $plugin ilGoogleAnalyticsPlugin
-		 */
-		$plugin = $this->getPluginObject();
-		$form = $this->initConfigurationForm($plugin);
+        /**
+         * @var $plugin ilGoogleAnalyticsPlugin
+         */
+        $plugin = $this->getPluginObject();
+        $form = $this->initConfigurationForm($plugin);
 
-		// get binary
-		$account_id = $plugin->getAccountId();
-		if ($account_id == NULL) {
-			ilUtil::sendFailure($plugin->txt("warning_no_account_id"));
-		}
+        // get binary
+        $account_id = $plugin->getAccountId();
+        if ($account_id == null) {
+            ilUtil::sendFailure($plugin->txt("warning_no_account_id"));
+        }
 
-		// set all plugin settings values
-		$val = array();
-		$val["account_id"] = $account_id;
-		$form->setValuesByArray($val);
+        // set all plugin settings values
+        $val = array();
+        $val["account_id"] = $account_id;
+        $form->setValuesByArray($val);
 
-		$DIC->ui()->mainTemplate()->setContent($form->getHTML());
-	}
-
-
-	/**
-	 * Save form input
-	 *
-	 */
-	public function save()
-	{
-		global $DIC;
-
-		/**
-		 * @var $plugin ilGoogleAnalyticsPlugin
-		 */
-		$plugin = $this->getPluginObject();
-		$form = $this->initConfigurationForm($plugin);
-
-		if ($form->checkInput()) {
-			$plugin->setAccountId($_POST["account_id"]);
-
-			ilUtil::sendSuccess($DIC->language()->txt("saved_successfully"), true);
-			$DIC->ctrl()->redirect($this, "configure");
-		} else {
-			$form->setValuesByPost();
-			$DIC->ui()->mainTemplate()->setContent($form->getHtml());
-		}
-	}
+        $DIC->ui()->mainTemplate()->setContent($form->getHTML());
+    }
 
 
-	/**
-	 * Init configuration form.
-	 *
-	 * @param $plugin ilGoogleAnalyticsPlugin
-	 *
-	 * @return object form object
-	 * @access public
-	 */
-	private function initConfigurationForm($plugin)
-	{
-		global $DIC;
+    /**
+     * Save form input
+     *
+     */
+    public function save()
+    {
+        global $DIC;
 
-		include_once("Services/Form/classes/class.ilPropertyFormGUI.php");
-		$form = new ilPropertyFormGUI();
-		$form->setTableWidth("100%");
-		$form->setTitle($plugin->txt("plugin_configuration"));
-		$form->setFormAction($DIC->ctrl()->getFormAction($this));
+        /**
+         * @var $plugin ilGoogleAnalyticsPlugin
+         */
+        $plugin = $this->getPluginObject();
+        $form = $this->initConfigurationForm($plugin);
 
-		// account id
-		$input = new ilTextInputGUI($plugin->txt("account_id"), "account_id");
-		$input->setRequired(true);
-		$input->setValue($plugin->getAccountId());
-		$input->setInfo($plugin->txt("account_id_info"));
-		$form->addItem($input);
+        if ($form->checkInput()) {
+            $plugin->setAccountId($_POST["account_id"]);
 
-		$form->addCommandButton("save", $DIC->language()->txt("save"));
+            ilUtil::sendSuccess($DIC->language()->txt("saved_successfully"), true);
+            $DIC->ctrl()->redirect($this, "configure");
+        } else {
+            $form->setValuesByPost();
+            $DIC->ui()->mainTemplate()->setContent($form->getHtml());
+        }
+    }
 
-		return $form;
-	}
+
+    /**
+     * Init configuration form.
+     *
+     * @param $plugin ilGoogleAnalyticsPlugin
+     *
+     * @return object form object
+     * @access public
+     */
+    private function initConfigurationForm($plugin)
+    {
+        global $DIC;
+
+        include_once("Services/Form/classes/class.ilPropertyFormGUI.php");
+        $form = new ilPropertyFormGUI();
+        $form->setTableWidth("100%");
+        $form->setTitle($plugin->txt("plugin_configuration"));
+        $form->setFormAction($DIC->ctrl()->getFormAction($this));
+
+        // account id
+        $input = new ilTextInputGUI($plugin->txt("account_id"), "account_id");
+        $input->setRequired(true);
+        $input->setValue($plugin->getAccountId());
+        $input->setInfo($plugin->txt("account_id_info"));
+        $form->addItem($input);
+
+        $form->addCommandButton("save", $DIC->language()->txt("save"));
+
+        return $form;
+    }
 }
